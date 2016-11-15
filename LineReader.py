f2571ec5438ecf5101d88607164476dc66f96f0f
Cameron Carlin <Cameron@Camerons-MacBook-Pro.local>
2016-11-15 13:44:11 -0800

    Deleted House data, inserted Simpsons Data reader

---
+++ LineReader.py
@@ -0,0 +1,21 @@
+import csv
+import sys
+
+def readcsv(filename):
+    templist = []
+    with open(filename) as f:
+        reader = csv.reader(f)
+        headers = next(reader, None)
+        for row in reader:
+            templist.append(row)
+    return headers, templist
+
+headers, ScriptData = readcsv('simpsons_script_lines.csv')
+print headers
+print ScriptData[0]
+
+#TODO
+# remove the ones where speaking_line (6th column) == False
+# replace character_id with the actual character names from the character csv?
+# same with location
+#use character as Y, use location, normalized text, and word count to create features (also maybe episode id?)
