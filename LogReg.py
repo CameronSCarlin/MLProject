77e87dd97d1af319c9582b13408654e78a139fe3
mapalmer2 <mapalmer2@dons.usfca.edu>
2016-11-18 15:47:13 -0800

    Update LogReg.py

--- LogReg.py
+++ LogReg.py
@@ -1,3 +1,21 @@
 ###################################
 ####### Logistic Regression #######
 ###################################
+
+def LogReg(*arg):
+    if len(arg) < 3:
+        print "Needs trainX, testX, trainY at a minimum"
+        return
+    if len(arg) == 3:
+        model = LogisticRegression()
+        model.fit(arg[0],arg[2])
+        prediction = model.predict(arg[1])
+        return prediction
+    if len(arg) == 4:
+        model = LogisticRegression()
+        model.fit(arg[0],arg[2])
+        prediction = model.predict(arg[1])
+        return prediction
+    if len(arg) > 4:
+        print "Too many arguments for current implementation"
+        return
