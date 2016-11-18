eeb322845bf0b1e11c5417fea7fadd9bf03b0131
mapalmer2 <mapalmer2@dons.usfca.edu>
2016-11-18 15:51:59 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,15 @@
 ###############################
 
 import Validations, LogReg, KNNClass, RForest, SuppVec, Features
+import pandas as pd
 
+file = "simpsons_characters.csv"
+file2 = "simpsons_episodes.csv"
+file3 = "simpsons_locations.csv"
+file4 = "simpsons_script_lines.csv"
+
+
+characters = pd.read_csv(file)
+episodes = pd.read_csv(file2)
+locations = pd.read_csv(file3)
+lines = pd.read_csv(file4, low_memory=False)
