d81857251f650ec92ab12f9efd5d4fc607e54547
Cameron Carlin <Cameron@Camerons-MacBook-Pro.local>
2016-11-18 15:51:10 -0800

    SuppVec needs to be finished

--- SuppVec.py
+++ SuppVec.py
@@ -1,3 +1,13 @@
 ######################################
 ####### Support Vector Machine #######
 ######################################
+
+## Nothing in here is tested, just a warning...
+
+from sklearn import svm
+
+def SVMfn(Xtrain, Xtest, Ytrain):
+    clf = svm.SVC()
+    clf.fit(Xtrain, Ytrain)
+
+## TODO not finished...
