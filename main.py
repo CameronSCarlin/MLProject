e32a80b07022207f3123e2eae7ecc1e4d2d9bc6c
Bradley Cameron Kenstler <bckenstler@usfca.edu>
2016-11-18 16:33:38 -0800

    updated main to pull correct subset of data and pass to feature extractor

--- main.py
+++ main.py
@@ -16,3 +16,10 @@ episodes = pd.read_csv(file2)
 locations = pd.read_csv(file3)
 lines = pd.read_csv(file4, low_memory=False, error_bad_lines=False)
 lines = lines.iloc[ : , :13]
+
+#subset of data we need
+data = lines[lines.speaking_line=='true'][['location_id', 'normalized_text','character_id']].dropna()
+
+target = data['character_id']
+
+features = extract_feature(data)
